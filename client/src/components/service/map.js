dad4f64ae02a2883490bb885dc2e09414409c999
Buggy <dbgml1007589@naver.com>
2023-10-10 23:49:52 +0900

    add map page

---
+++ client/src/components/service/map.js
@@ -0,0 +1,21 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import classnames from "classnames";
+
+import styled from "styled-components";
+
+// image
+
+// splited page
+
+function Map() {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div>map 페이지 입니다.</div>
+    </>
+  );
+}
+
+export default Map;
