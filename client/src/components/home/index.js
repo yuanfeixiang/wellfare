8fa35d7d7f8c53b5595c323187e926ef5f44a547
Buggy <dbgml1007589@naver.com>
2023-11-21 21:11:00 +0900

    homeServiceIntro desktop version complete

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -5,6 +5,8 @@ import classnames from "classnames";
 
 // image
 import phone from "../../image/util/phone.png";
+import searchFilter from "../../image/util/searchFilter.png";
+import searchResult from "../../image/util/searchResult.png";
 
 // store
 
@@ -15,7 +17,7 @@ function Home() {
   return (
     <>
       <Helmet>
-        <title>Wellfare :: 최고의 복지</title>
+        <title>Wellfare :: 최고의 복지</title>s
       </Helmet>
       <HomeBanner />
       <div className="homeIntroContainer">
@@ -27,27 +29,52 @@ function Home() {
           Wellfare와 함께하세요.
         </span>
       </div>
-      {/* <div className="homeServiceIntroContainer">
+      <div className="homeServiceIntroContainer">
         <div className="homeServiceIntroBox">
           <div className="homeServiceIntroTitleBox">
-            <span className="homeServiceIntroTitle">복지서비스 검색</span>
+            <span className="homeServiceIntroTitle">복지서비스 조회</span>
           </div>
-          <div className="homeServiceIntroLeftPhoneBox">
-            <img
-              src={phone}
-              className="homeServiceIntroLeftPhone"
-              alt="왼쪽 핸드폰"
-            />
+          <div className="homeSeriveIntroDescBox">
+            <span className="homeServiceIntroDesc">
+              무관심했던 복지서비스, <br /> 빠짐없이 조회하고 <br /> 관리하세요.
+            </span>
           </div>
-          <div className="homeServiceIntroRightPhoneBox">
-            <img
-              src={phone}
-              className="homeServiceIntroRightPhone"
-              alt="오른쪽 핸드폰"
-            />
+          <div className="homeServiceIntroPhoneContainer">
+            <div className="homeServiceIntroLeftPhoneBox">
+              <img
+                className="homeServiceIntroLeftPhoneImg"
+                src={searchFilter}
+                alt="검색필터화면"
+              />
+              <img
+                src={phone}
+                className="homeServiceIntroLeftPhone"
+                alt="왼쪽 핸드폰"
+              />
+            </div>
+            <div className="homeServiceIntroRightPhoneBox">
+              <img
+                className="homeServiceIntroRightPhoneImg"
+                src={searchResult}
+                alt="검색필터화면"
+              />
+              <img
+                src={phone}
+                className="homeServiceIntroRightPhone"
+                alt="오른쪽 핸드폰"
+              />
+            </div>
+          </div>
+          <div className="homeSeriveIntroSubDescBox">
+            <span className="homeServiceIntroSubDesc">
+              원하는 서비스를 저장하세요.
+              <br />
+              로그인 없이도 나만의 서비스를 <br />
+              모아 볼 수 있어요.
+            </span>
           </div>
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
