200fe4affb687f53391d8b3b121424c5de8a62a8
Buggy <dbgml1007589@naver.com>
2023-12-01 11:23:48 +0900

    modify something css and server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 const api = require("./routes/index");
 const compression = require("compression");
